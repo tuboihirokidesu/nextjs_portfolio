1f090eba870d788add4de81d4b3fecfa03a86f91
tuboihirokidesu <tuboi.hiroki@icloud.com>
2021-04-02 16:54:29 +0900

    detele project 4

--- src/data/projectCardData.ts
+++ src/data/projectCardData.ts
@@ -32,14 +32,6 @@ const projectCardData: ICardData[] = [
     text: 'Appleカレンダー',
     label: 'moment.js',
     path: './project/calender'
-  },
-  {
-    id: '4',
-    src:
-      'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80',
-    text: 'World Ranks',
-    label: 'CSS',
-    path: './project/world-ranks'
   }
 ]
 
