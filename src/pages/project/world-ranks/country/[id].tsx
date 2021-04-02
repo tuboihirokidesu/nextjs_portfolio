edfe3b030eca6a59741c72385e8e9d118c001c90
tuboihirokidesu <tuboi.hiroki@icloud.com>
2021-04-02 12:38:20 +0900

    change getServerSide to getStatic

--- src/pages/project/world-ranks/country/[id].tsx
+++ src/pages/project/world-ranks/country/[id].tsx
@@ -1,12 +1,18 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
 import React from 'react'
 import axios from 'axios'
 import Layout from '../../../../components/Layout'
 import CountryPage from '../../../../components/Project/WorldRank/CountryPage/CountryPage'
 
-const Country = ({ country }) => {
-  console.log(country)
+const getCountry = async id => {
+  const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${id}`)
 
+  const country = await res.json()
+
+  return country
+}
+
+const Country: React.FC<GetStaticProp> = ({ country }) => {
   return (
     <Layout title={country.name}>
       <CountryPage country={country} />
@@ -16,11 +22,21 @@ const Country = ({ country }) => {
 
 export default Country
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const res = await axios.get(
-    `https://restcountries.eu/rest/v2/alpha/${params.id}`
-  )
-  const country = await res.data
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await axios.get('https://restcountries.eu/rest/v2/all')
+  const countries = await res.data
+  const paths = countries.map(country => ({
+    params: { id: country.alpha3Code }
+  }))
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const country = await getCountry(params.id)
 
   return {
     props: {
@@ -28,3 +44,5 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     }
   }
 }
+
+type GetStaticProp = InferGetStaticPropsType<typeof getStaticProps>
--- src/pages/project/world-ranks/index.tsx
+++ src/pages/project/world-ranks/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import Layout from '../../../components/Layout'
-import Link from 'next/link'
 import styled from 'styled-components'
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import axios from 'axios'
