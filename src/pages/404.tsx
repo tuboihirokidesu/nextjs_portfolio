9ee934fa85e3c4e80c0e79a1fcfec7996489e23a
tuboihirokidesu <tuboi.hiroki@icloud.com>
2021-04-17 11:09:38 +0900

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { NextPage } from 'next'
+
+const NotFound: NextPage = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/')
+    }, 4000)
+  }, [])
+
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h2>Oops! That page cannot be found :(</h2>
+      <p>
+        Redirecting to{' '}
+        <Link href="/">
+          <a>Homepage</a>
+        </Link>{' '}
+        for more marmite goodness...
+      </p>
+
+      <style jsx>{`
+        .not-found {
+          background: #fff;
+          padding: 30px;
+          box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+          transform: rotateZ(-1deg);
+        }
+        h1 {
+          font-size: 3em;
+        }
+      `}</style>
+    </div>
+  )
+}
+export default NotFound
